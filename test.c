c1849af6e447aa71594016c3f006e8ef863a0eb8
Milo-sipper <you@example.com>
2026-01-16 18:09:13 +0800

    go fish

--- test.c
+++ test.c
@@ -4,14 +4,15 @@ char name[50];
 int age; 
 int height; 
 void function(char name[], int age, int height ){
-    printf("%s %d %d", name, age, height);
+    // printf("%s %d %d", name, age, height);
+    printf("you are %s, %d years old and your height is %d", name, age, height);
     return;
 }
 
 int main(void){
     printf("%s", "state your name, age and height");
     scanf("%s %d %d", name, &age, &height);
-    function(name, age, height);
+    function(name, age, height);  
 return 0;
 }
 
